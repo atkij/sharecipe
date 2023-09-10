84bfc22e4df5da9eab5e2fa4bc038ed8fdb4d96b
Joel Atkinson <joel@atkinson-family.org>
2023-09-10 20:58:08 +0100

    Updated name of database file

--- sharecipe/__init__.py
+++ sharecipe/__init__.py
@@ -9,7 +9,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
             SECRET_KEY='dev',
-            DATABASE=os.path.join(app.instance_path, 'website.db'),
+            DATABASE=os.path.join(app.instance_path, 'sharecipe.db'),
             UPLOAD_FOLDER=os.path.join(app.instance_path, 'uploads'),
             )
 
